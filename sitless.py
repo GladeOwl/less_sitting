9981cb24b622ca087587af06520dcfa9b74482ae
Bhavesh Patil <dadu.patil@gmail.com>
2022-10-15 20:59:13 +0530

    added timestamps; changed grace message

--- sitless.py
+++ sitless.py
@@ -1,3 +1,4 @@
+import datetime
 from time import sleep
 from notification import send_windows_notification, send_telegram_notification
 from ctypes import Structure, windll, c_uint, sizeof, byref
@@ -9,6 +10,7 @@ GRACE_PERIOD: int = 10
 FORCE_BREAK: bool = False
 KEEP_RUNNING: bool = True
 IDLE: bool = True
+REPEAT_NAG: bool = False
 
 
 class LASTINPUTINFO(Structure):
@@ -30,31 +32,40 @@ current_grace: int = 0
 current_duration: int = 0
 previous_idle_duration: float = 0
 while KEEP_RUNNING:
+    now: str = f"[{str(datetime.datetime.now().strftime('%I:%M:%S'))}]"
     idle_duration: float = get_idle_duration()
 
     if not IDLE and idle_duration > BREAK_DURATION:
         IDLE = True
         current_duration = 0
-        print("No Activity Dectected. We're now IDLE.")
+        print(f"{now} No Activity Dectected. We're now IDLE.")
     elif IDLE and idle_duration < BREAK_DURATION:
         IDLE = False
-        print(f"Activity Detected. We were idle for {previous_idle_duration} seconds")
+        print(
+            f"{now} Activity Detected. We were idle for {previous_idle_duration} seconds"
+        )
 
     if not IDLE and current_duration >= ALLOWED_SITTING_DURATION:
-        if current_grace >= GRACE_PERIOD and idle_duration < GRACE_PERIOD:
-            print("Grace Period Expired.")
-            current_grace = 0
-            FORCE_BREAK = False
-
         if not FORCE_BREAK:
             FORCE_BREAK = True
             print(
-                "Duration Exceeded Allowed Sitting Time or Grace Period. Sending Notification."
+                f"{now} Duration Exceeded Allowed Sitting Time or Grace Period. Sending Notification."
             )
             send_windows_notification()
             send_telegram_notification(
-                "Take a Break. You've been sitting for a quite a while."
+                f"{now} Take a Break. You've been sitting for a quite a while."
             )
+
+        if current_grace >= GRACE_PERIOD and idle_duration < GRACE_PERIOD - 3:
+            current_grace = 0
+            REPEAT_NAG = True
+            # print(f"{now} Grace Period Expired.")
+
+        if REPEAT_NAG:
+            print(f"{now} No Touchy! Come on!")
+            send_windows_notification()
+            send_telegram_notification(f"{now} No Touchy! Get off now!")
+            REPEAT_NAG = False
 
         current_grace += 1
 
@@ -63,7 +74,7 @@ while KEEP_RUNNING:
 
     if FORCE_BREAK and IDLE:
         FORCE_BREAK = False
-        send_telegram_notification("We're good. You may sit again.")
+        send_telegram_notification(f"{now} We're good. You may sit again.")
 
     previous_idle_duration = idle_duration
     sleep(1)
---
+++ time_test.py
@@ -0,0 +1,5 @@
+import datetime
+
+time = datetime.datetime.now()
+now = time.strftime("%I:%M:%S")
+print(now)
