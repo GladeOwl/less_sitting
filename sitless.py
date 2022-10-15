492e151145aac13ece2cce65a01be3450c375aba
Bhavesh Patil <dadu.patil@gmail.com>
2022-10-15 16:21:35 +0530

    added grace period for more nagging

--- sitless.py
+++ sitless.py
@@ -4,7 +4,7 @@ from ctypes import Structure, windll, c_uint, sizeof, byref
 
 ALLOWED_SITTING_DURATION: int = 3300
 BREAK_DURATION: int = 300
-CURRENT_DURATION: int = 0
+GRACE_PERIOD: int = 10
 
 FORCE_BREAK: bool = False
 KEEP_RUNNING: bool = True
@@ -26,28 +26,39 @@ def get_idle_duration():
     return millis / 1000.0
 
 
+current_grace: int = 0
+current_duration: int = 0
 previous_idle_duration: float = 0
 while KEEP_RUNNING:
     idle_duration: float = get_idle_duration()
 
     if not IDLE and idle_duration > BREAK_DURATION:
         IDLE = True
-        CURRENT_DURATION = 0
+        current_duration = 0
         print("No Activity Dectected. We're now IDLE.")
     elif IDLE and idle_duration < BREAK_DURATION:
         IDLE = False
         print(f"Activity Detected. We were idle for {previous_idle_duration} seconds")
 
-    if not IDLE and not FORCE_BREAK and CURRENT_DURATION >= ALLOWED_SITTING_DURATION:
-        FORCE_BREAK = True
-        print("Duration Exceeded Allowed Sitting Time. Sending Notification.")
-        send_windows_notification()
-        send_telegram_notification(
-            "Take a Break. You've been sitting for a quite a while."
-        )
+    if not IDLE and not FORCE_BREAK and current_duration >= ALLOWED_SITTING_DURATION:
+        if current_grace >= GRACE_PERIOD:
+            current_grace = 0
+            FORCE_BREAK = False
+
+        if not FORCE_BREAK:
+            FORCE_BREAK = True
+            print(
+                "Duration Exceeded Allowed Sitting Time or Grace Period. Sending Notification."
+            )
+            send_windows_notification()
+            send_telegram_notification(
+                "Take a Break. You've been sitting for a quite a while."
+            )
+
+        current_grace += 1
 
     if not IDLE:
-        CURRENT_DURATION += 1
+        current_duration += 1
 
     if FORCE_BREAK and IDLE:
         FORCE_BREAK = False
